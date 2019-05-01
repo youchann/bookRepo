97aa277bd846d0f794080b7b45fa8e70742e751f
yutaro1031 <yutaro50@gmail.com>
2019-05-01 14:10:47 +0900

    形態素解析

---
+++ src/samples/mecab_sample.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+import MeCab
+from pprint import pprint
+
+
+def get_stop_words():
+    with open('/src/src/samples/Japanese.txt', encoding='utf-8') as fd:
+        words = fd.read().splitlines()
+        stop_words = frozenset(words)
+    return stop_words
+
+def analyze_review(review, stop_words):
+    node = m.parseToNode(review)
+    result = []
+    # 正規化をもう少しやりたい
+    while node:
+        hinshi = node.feature.split(",")[0]
+        if hinshi in ['名詞', '動詞', '形容詞', '感動詞', '副詞']:
+            result.append(node.surface)  # 語幹に変換
+            # result.append(node.feature.split(",")[6]) # そのまま
+        node = node.next
+
+    result = list(filter(lambda x: x not in stop_words, result))
+    return result
+
+
+m = MeCab.Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd")
+m.parse("")
+
+all_reviews_result = []
+
+# テキストファイルを開く(実際は配列にまとまったレビュー群がくるのが理想)
+with open('/src/src/samples/sample_reviews.txt', encoding='utf-8') as fh:
+    stop_words = get_stop_words()
+    for line in fh:
+        one_review = line.strip()
+        all_reviews_result.append(analyze_review(one_review, stop_words))
+
+pprint(all_reviews_result)
+
+# LDAを構築したい
+# →品詞選定
+# →辞書使えているか確認
+# →ストップワードを定義
+# →分割した配列にする
+# →サンプルになぞってndaを構築
+# →各トピックの頻度を取得
+# DB保存までを行う
+
+# あとはid5個連続で解析できれば問題なし
+# ジャンルを求めるフローを確立
+# 大量のidを取得する
--- src/samples/scraping.py
+++ src/samples/scraping.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 base_url = "https://bookmeter.com"
 params_to_search = "&partial=true&sort=recommended&type=japanese&page=1"
 
-keyword = "ビジネスモデル全史 (ディスカヴァー・レボリューションズ)"
+keyword = "ノルウェイの森 上 (講談社文庫)"
 keyword = urllib.parse.quote(keyword)
 
 url_to_get_id = base_url + "/search?keyword=" + keyword + params_to_search
@@ -19,42 +19,45 @@ html = f.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
 
-def getParamsToGetReview(offset = 0):
+def get_params_to_get_review(offset = 0):
     params = {
         'review_filter': 'none',
         'offset': str(offset),
-        'limit': '50'
+        'limit': '100'
     }
     return params
 
-def getJsonFromRequest(url, params):
+def get_json_from_request(url, params):
     r = requests.get(url, params=params)
     print(r)
     # ここで例外処理を入れる
     return r.json()
 
-def getReviews(path_to_detail):
+def get_reviews(path_to_detail):
     # APIに問い合わせるURL
     url_to_get_review = base_url + path_to_detail + "/reviews.json"
     offset = 0
 
     while True:
         # 特定の本のレビューをAPIから取得
-        params_to_get_review = getParamsToGetReview(offset)
-        json_data = getJsonFromRequest(url_to_get_review, params_to_get_review)
+        params_to_get_review = get_params_to_get_review(offset)
+        json_data = get_json_from_request(url_to_get_review, params_to_get_review)
 
         # resourcesの中でfor文を回す→print
         if json_data['resources']:
-            for i in json_data['resources']:
-                print(i['content_tag'])
-            offset += 50
-            time.sleep(9)
+            with open('src/src/samples/sample_reviews.txt', mode='a', encoding='utf-8') as f:
+                for i in json_data['resources']:
+                    f.write(i['content_tag'] + "\n")
+                print("書き込み完了, offset:", offset)
+
+            offset += 100
+            time.sleep(10)
         else:
             print('取得終了')
             break
 
 # 検索結果に本が存在するか判定
 if soup.select('a[href*="/books/"]'):
-    getReviews(soup.select('a[href*="/books/"]')[0].get('href'))
+    get_reviews(soup.select('a[href*="/books/"]')[0].get('href'))
 else:
     print(soup.select('a[href*="/books/"]'))
