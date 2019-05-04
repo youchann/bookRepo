90ad7d06e87c2677b3bac41858bf0967f2442ca4
yutaro1031 <yutaro50@gmail.com>
2019-05-04 12:10:30 +0900

    レビュー数が1000以上の本だけを抽出

--- src/production/modules/bookmeters.py
+++ src/production/modules/bookmeters.py
@@ -1,27 +1,25 @@
 from bs4 import BeautifulSoup
 import requests, time
-import config
+from retry import retry
+import modules.config as config
 
 
 def get_books():
+    url = 'https://bookmeter.com/reviews.json'
     session_id = login()
     cookies = dict(_session_id_elk=session_id)
     offset = 0
-    fail_count = 0
 
     while True:
         params = get_params(offset)
-        r = requests.get('https://bookmeter.com/reviews.json', params=params, cookies=cookies)
+        json_data = json_from_request(url, params, cookies)
 
-        if r.status_code == requests.codes.ok:
-            print(r.text)
+        book_ids = get_book_ids_from_json(json_data)
+        print(book_ids)
+        book_ids = narrow_ids(book_ids)
+        print('抽出id: ', book_ids)
 
-        else:
-            fail_count += 10
-            time.sleep(fail_count)
-            continue
-
-        if offset > 100:
+        if offset > 60:
             break
         else:
             offset += 20
@@ -36,7 +34,39 @@ def get_params(offset = 0):
     return params
 
 
-def judge_enough
+def get_book_ids_from_json(json):
+    book_ids = []
+    for i in json['resources']:
+        book_ids.append(i['contents']['book']['id'])
+
+    return book_ids
+
+
+def narrow_ids(book_ids):
+    # 先に楽天apiに問い合わせてスクリーニングする
+    narrowed_book_ids = judge_having_enough_reviews(book_ids)
+
+    return narrowed_book_ids
+
+
+def judge_having_enough_reviews(book_ids):
+    narrowed_book_ids = []
+    for id in book_ids:
+        url = "https://bookmeter.com/books/" + str(id) + "/reviews.json?review_filter=none&offset=1000&limit=1"
+        json_data = json_from_request(url)
+
+        if json_data['resources']: # 1000番目以降にレビューが存在するか
+            narrowed_book_ids.append(id)
+        time.sleep(10)
+
+    return narrowed_book_ids
+
+# 4回の失敗まで許容。失敗ごとに10*失敗回数分待つ
+@retry(tries=4, delay=10, backoff=2)
+def json_from_request(url, params = None, cookies = None):
+    r = requests.get(url, params=params, cookies=cookies)
+    print(r)
+    return r.json()
 
 
 def login():
