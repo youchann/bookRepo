d046c48581efb0e4759e7c4ada5add552160058d
yutaro1031 <yutaro50@gmail.com>
2020-08-16 11:42:43 +0900

    styled-componentに乗り換え

--- front-v2/src/index.tsx
+++ front-v2/src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom";
+import styled from "styled-components";
+import { ThemeProvider, createTheme, Typography } from "ingred-ui";
 import { Global } from "@emotion/core";
 
 import { globalStyle } from "./styles/globalStyle";
 
+const Hoge = styled.div``;
+
 const initialization = async () => {
+  const theme = createTheme();
   try {
     ReactDom.render(
-      <>
+      <ThemeProvider theme={theme}>
         <Global styles={globalStyle} />
-        <div>hoge</div>
-      </>,
+        <Hoge>hoge</Hoge>
+        <Typography>hoge</Typography>
+      </ThemeProvider>,
       document.getElementById("root"),
     );
   } catch (e) {
