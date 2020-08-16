854b4989bf6fb9d794401fea640eb19d2d79506d
yutaro1031 <yutaro50@gmail.com>
2020-08-16 11:06:45 +0900

    とりあえずserverでも提供できるように

---
+++ front-v2/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+router.get("*", async (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist/index.html"));
+});
+
+app.use("/", router);
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
