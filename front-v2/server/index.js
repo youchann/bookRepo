97fd2ccda74eadfb103648bd3b85e0ed1621e6d7
yutaro1031 <yutaro50@gmail.com>
2020-09-16 11:17:28 +0900

    tweak

--- front-v2/server/index.js
+++ front-v2/server/index.js
@@ -15,6 +15,6 @@ router.get("*", async (_req, res) => {
 
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`listening on port ${process.env.PORT || 3000}`);
 });
