e84e5c94c08399426c8434900f1bdc255fd5aad2
yutaro1031 <yutaro50@gmail.com>
2020-09-16 16:18:48 +0900

    とりあえず100単語suggestしてみた

--- front-v2/src/components/pages/Search/styled.ts
+++ front-v2/src/components/pages/Search/styled.ts
@@ -5,3 +5,24 @@ export const InputContainer = styled.div`
     width: 400px;
   }
 `;
+
+export const KeywordsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1000px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => `${theme.spacing * 0.75}px ${theme.spacing}px`};
+  border: 0;
+  cursor: pointer;
+  border-radius: ${({ theme }) => theme.radius}px;
+  background-color: ${({ theme }) => theme.palette.background.hint};
+  white-space: nowrap;
+  transition: all 0.3s;
+  &:hover {
+    background-color: ${({ theme }) => theme.palette.background.active};
+  }
+`;
