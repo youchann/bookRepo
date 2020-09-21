f5b32945e4c3f1008f39880ab6bf738effe4aefd
yutaro1031 <yutaro50@gmail.com>
2020-09-21 11:48:51 +0900

    名詞トピック選択ページのエラーハンドリング

--- front-v2/src/components/pages/SelectNounTopics/SelectNounTopics.tsx
+++ front-v2/src/components/pages/SelectNounTopics/SelectNounTopics.tsx
@@ -15,7 +15,7 @@ const SelectNounTopics: React.FunctionComponent = () => {
   const inputedWord = parsed["word"] as string;
   if (typeof inputedWord !== "string") history.replace("/");
 
-  const [requesting, setRequesting] = React.useState<boolean>(false);
+  const [requesting, setRequesting] = React.useState<boolean>(true);
   // NOTE: [book_id: number, words: string[]][]
   const [topics, setTopic] = React.useState<
     Response.ShowNounTopics["noun_topics"]
@@ -27,11 +27,14 @@ const SelectNounTopics: React.FunctionComponent = () => {
   React.useEffect(() => {
     const getTopics = async () => {
       setRequesting(true);
-      const res = await client.getNounTopics({
-        inputed_word: inputedWord,
-      });
-      setTopic(res.data.noun_topics);
-      setRequesting(false);
+      try {
+        const res = await client.getNounTopics({
+          inputed_word: inputedWord,
+        });
+        setTopic(res.data.noun_topics);
+      } finally {
+        setRequesting(false);
+      }
     };
     getTopics();
   }, [inputedWord]);
@@ -51,7 +54,11 @@ const SelectNounTopics: React.FunctionComponent = () => {
   function handleClickButton() {
     const bookIds: Set<number> = new Set([]);
     selectedIndex.forEach((index) => bookIds.add(topics[index][0]));
-    history.push(`/selectBooks?book_ids=${Array.from(bookIds).join(",")}`);
+    history.push(`/selectBooks?book_ids=${Array.from(bookIds).join(",")},`); // MEMO: 単一のbook_idでも配列としてparseするために末尾に,をつける
+  }
+
+  function handleBack() {
+    history.push(`/search`);
   }
 
   return (
@@ -61,28 +68,48 @@ const SelectNounTopics: React.FunctionComponent = () => {
           あなたが欲しい本に近いイメージの単語群をなるべく多く選択してください
         </Typography>
         <Spacer pt={6} />
+        {/* eslint-disable-next-line no-nested-ternary */}
         {requesting ? (
           <Spinner />
+        ) : topics.length === 0 ? (
+          <>
+            <Typography
+              size="xxxl"
+              color="secondary"
+              lineHeight="2"
+              align="center"
+            >
+              単語群が見つかりませんでした
+              <br />
+              検索画面からやり直してください
+            </Typography>
+            <Spacer pt={6} />
+            <Button inline onClick={handleBack}>
+              戻る
+            </Button>
+          </>
         ) : (
-          topics.map((topic, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <Spacer key={index} py={2}>
-              <TopicCard
-                checked={selectedIndex.has(index)}
-                words={topic[1]}
-                onClick={createHandleSelect(index)}
-              />
-            </Spacer>
-          ))
+          <>
+            {topics.map((topic, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <Spacer key={index} py={2}>
+                <TopicCard
+                  checked={selectedIndex.has(index)}
+                  words={topic[1]}
+                  onClick={createHandleSelect(index)}
+                />
+              </Spacer>
+            ))}
+            <Spacer pt={6} />
+            <Button
+              inline
+              disabled={selectedIndex.size === 0}
+              onClick={handleClickButton}
+            >
+              決定
+            </Button>
+          </>
         )}
-        <Spacer pt={6} />
-        <Button
-          inline
-          disabled={selectedIndex.size === 0}
-          onClick={handleClickButton}
-        >
-          決定
-        </Button>
       </Flex>
     </Spacer>
   );
