d5c159c591d815f2a8050973161cb73bf95a5654
yutaro1031 <yutaro50@gmail.com>
2020-09-30 13:05:35 +0900

    いらないアンケート項目削除(リモートDB反映済み

--- mysql/sqls/005-add-evaluation-tables.sql
+++ mysql/sqls/005-add-evaluation-tables.sql
@@ -10,12 +10,11 @@ create table IF not exists `evaluation_items`
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8;
 
-INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("推薦妥当性", "あなたはこの本に興味がありますか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("インタフェース妥当性", "インタフェースは分かりやすいですか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("説明性", "このシステムは自身に本の推薦理由を説明してくれましたか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("情報充足性", "このシステムは自身が本の良し悪しを判断するのに十分な情報を提供してくれましたか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("制御性", "システム上で自身が行った操作に応じて推薦内容を変えてくれていると感じましたか?");
-INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("知覚有用性", "システムを利用して、興味のある本に容易にたどり着けましたか?");
+INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("知覚有用性", "システムを利用して、興味のある本にたどり着けましたか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("総合満足度", "あなたは全体としてこのシステムに満足していますか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("システム信頼度", "このシステムはあなたにとって信頼できるものですか?");
 INSERT INTO `evaluation_items`(`item`, `description`) VALUES ("システム利用意図", "あなたはこのシステムをまた利用したいと思えますか?");
